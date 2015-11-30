3354483221606cc6e2853373a13fa76d2f474258
Chua Jie Sheng <jschua@thoughtworks.com>
2015-12-01 00:07:06 +0800

    Complete dino game

--- dino.rkt
+++ dino.rkt
@@ -5,17 +5,17 @@
 ;; constants
 (define DINO-X 5)
 (define DINO-Y 20)
-(define TICK-RATE 1/10)
+(define TICK-RATE 1/20)
 (define SCREEN-WIDTH 500)
 (define SCREEN-HEIGHT 400)
 (define MAX-JUMP (/ SCREEN-HEIGHT 2))
-(define DINO-UP-DOWN-RATE 10)
-(define CACTUS-MOVE-RATE 5)
+(define DINO-UP-DOWN-RATE 4)
+(define CACTUS-MOVE-RATE 1)
 (define MT-SCENE (empty-scene SCREEN-WIDTH SCREEN-HEIGHT))
 (define DINO-IMG  (bitmap "resources/trex.png"))
 (define SEG-SIZE 10)
 (define CACTUS-IMG  (bitmap "resources/cactus.png"))
-(define ENDGAME-TEXT-SIZE 10)
+(define ENDGAME-TEXT-SIZE 20)
 
 ;; data structure of the dino game
 ;; cactuses is a list of posn where the cactus is currently
@@ -29,8 +29,8 @@
 ;; big bang
 (define (start-dino-game)
   (define new-screen (screen (dino (posn DINO-X DINO-Y) #f)
-                             (list (fresh-cactus)
-                                   (fresh-cactus))))
+                             (list (fresh-cactus) (fresh-cactus)
+                                   )))
   (big-bang new-screen
             (on-tick next-screen TICK-RATE)
             (on-key jump-dino)
@@ -38,13 +38,13 @@
             (stop-when dead? render-end)))
 
 (define (fresh-cactus)
-  (posn (random SCREEN-WIDTH) 0)
+  (define new-cactus (posn (random SCREEN-HEIGHT) DINO-Y))
+  new-cactus
   )
 
 ;; clock ticks
 (define (next-screen current-screen)
-  ;; (print current-screen)
-  (printf "[next-screen] current-screen = ~a \n" current-screen)
+  ;; (printf "[next-screen] current-screen = ~a \n" current-screen)
   (define current-dino (screen-dino current-screen))
   (define dino-position (dino-posn current-dino))
   (define dino-is-jumping (dino-jumping current-dino))
@@ -52,49 +52,38 @@
   (define cactuses (screen-cactuses current-screen))
   
   (cond [(dino-jumping current-dino)
-         (define dino-posn-y (posn-y current-dino))
-         (cond [(> dino-posn-y MAX-JUMP)
-                (screen (dino-coming-down current-dino) (update-cactuses cactuses))
-                ]
-               [else
-                (screen (dino-going-up current-dino) (update-cactuses cactuses))
-                ])]
+         ;; dino is jumping
+         (define dino-posn-y (posn-y (dino-posn current-dino)))
+         (screen (dino-going-up current-dino) (update-cactuses cactuses))]
         [else
-         (define new-dino (dino-coming-down current-dino))
-         (printf "[next-screen] new-dino = ~a \n" new-dino)
-         (define new-cactuses (update-cactuses cactuses))
-         (printf "[next-screen] new-cactuses = ~a \n" new-cactuses)
-         (screen new-dino new-cactuses)]
-        )
+         ;; dino coming down
+         (screen (dino-coming-down current-dino) (update-cactuses cactuses))])
   )
 
 (define (dino-coming-down current-dino)
-  (printf "[dino-coming-down] dino = ~a \n" current-dino)
   (define dino-position (dino-posn current-dino))
-  (cond [(< (posn-y dino-position) (+ DINO-Y MAX-JUMP))
-         (dino (posn DINO-X DINO-Y) (dino-jumping current-dino))]
-        [else
-         (define new-position (posn DINO-X (- (posn-y dino-position) DINO-UP-DOWN-RATE)))
-         (printf "[dino-coming-down] new-position = ~a \n" new-position)
-         (define is-dino-jumping (dino-jumping current-dino))
-         (printf "[dino-coming-down] is-dino-jumping = ~a \n" is-dino-jumping)
-         (define new-dino (dino new-position is-dino-jumping))
-         (printf "[dino-coming-down] new-dino = ~a \n" new-dino) 
-         new-dino])
+  (define new-y (+ (posn-y dino-position) DINO-UP-DOWN-RATE))
+  (dino (posn DINO-X (if (> new-y DINO-Y) DINO-Y new-y)) (dino-jumping current-dino))
   )
 
 (define (update-cactuses cactuses)
   (cond [(empty? cactuses) empty]
-        [else cons (update-cactus (first cactuses)) (update-cactuses (rest cactuses))])
+        [else (cons
+               (update-cactus (first cactuses))
+               (update-cactuses (rest cactuses))
+               )])
   )
 
 (define (update-cactus cactus)
-  posn (posn-x cactus) (+ (posn-y cactus) CACTUS-MOVE-RATE)
+  (cond [(< (posn-x cactus) 0) (fresh-cactus)]
+        [else (posn (- (posn-x cactus) CACTUS-MOVE-RATE) (posn-y cactus))])
   )
 
-(define (dino-going-up dino)
-  (define dino-position (dino-posn dino))
-  dino (posn 5 (+ (posn-y dino-position) DINO-UP-DOWN-RATE)) (dino-jumping dino)
+(define (dino-going-up current-dino)
+  (define dino-position (dino-posn current-dino))
+  (define new-y (- (posn-y dino-position) DINO-UP-DOWN-RATE))
+  (define is-dino-jumping (if (positive? new-y) (dino-jumping current-dino) #f))
+  (dino (posn DINO-X new-y) is-dino-jumping)
   )
 
 ;; key-events
@@ -107,24 +96,21 @@
   (key=? key "up")
   )
 
-(define (make-dino-jump screen)
-  (define dino (screen-dino screen))
-  (define cactuses (screen-cactuses screen))
-  screen (dino (DINO-UP-DOWN-RATE (dino-posn dino)) #t) cactuses
-  )
+(define (make-dino-jump previous-screen)
+  (define jumping-dino (dino (dino-posn (screen-dino previous-screen)) #t))
+  (screen jumping-dino (screen-cactuses previous-screen))
+)
 
 ;; rendering
-(define (render-screen screen)
-  (cond [(screen? screen)
-         (printf "[render-scene] dino = ~a \n" (screen-dino screen))
-         (dino+scene (screen-dino screen)
-                     (cactus-list+scene (screen-cactuses screen) MT-SCENE))]
+(define (render-screen current-screen)
+  (cond [(screen? current-screen)
+         ;; (printf "[render-scene] dino = ~a \n" (screen-dino current-screen))
+         (dino+scene (screen-dino current-screen)
+                     (cactus-list+scene (screen-cactuses current-screen) MT-SCENE))]
         [else MT-SCENE])
   )
 
 (define (dino+scene dino scene)
-  (printf "[dino+scene] dino = ~a \n" dino)
-  ;; bug
   (define dino-position (dino-posn dino))
   (img+scene dino-position DINO-IMG scene)
   )
@@ -139,7 +125,7 @@
 (define (cactus-list+scene cactuses scene)
   (define (get-posns-from-cactus cactuses)
     (cond [(empty? cactuses) empty]
-          [else cons (first cactuses) (get-posns-from-cactus (rest cactuses))])
+          [else (cons (first cactuses) (get-posns-from-cactus (rest cactuses)))])
     )
   (img-list+scene (get-posns-from-cactus cactuses) CACTUS-IMG scene)
   )
@@ -151,7 +137,12 @@
 
 ;; end game
 (define (dead? screen)
-  #f
+  (define current-dino-position (dino-posn (screen-dino screen)))
+  (define (hit? dino-posn cactuses)
+    (cond [(empty? cactuses) #f]
+          [else (or (equal? dino-posn (first cactuses)) (hit? dino-posn (rest cactuses)))])
+    )
+  (hit? current-dino-position (screen-cactuses screen))
   )
 
 (define (render-end screen)
